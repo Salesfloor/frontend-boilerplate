88540dc1cd62a6c06cd46cf1657fc29ab978e07a
Mikechaos <mikechaos13@gmail.com>
2015-05-11 12:32:48 -0400

    SF-6269 - Add basic task to write to file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 //initialize all of our variables
-var app, base, gulp, gutil, gulpSequence, shell;
+var app, base, gulp, gutil, gulpSequence, shell, build_deps;
 
 //load all of our dependencies
 //add more here if you want to include more libraries
@@ -7,3 +7,32 @@ gulp        = require('gulp');
 gutil       = require('gulp-util');
 gulpSequence = require('gulp-sequence').use(gulp);
 shell       = require('gulp-shell');
+
+
+gulp.task('write-dep-file', shell.task([
+  'rm -f dependencies.js',
+  'touch dependencies.js;'
+]));
+
+gulp.task('write-deps', function () {
+  return string_src("dependencies.js", 'test123')
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('build-deps', gulpSequence(['write-dep-file', 'write-deps']));
+
+function string_src(filename, string) {
+  var src = require('stream').Readable({ objectMode: true })
+  src._read = function () {
+    this.push(new gutil.File({ cwd: "", base: "", path: filename, contents: new Buffer(string) }))
+    this.push(null)
+  }
+  return src
+}
+
+build_deps = {
+  build_deps_from_string: function () {
+
+  },
+
+}
