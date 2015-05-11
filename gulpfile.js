3136f8f6150332c5216f06138550666b87a19577
Mikechaos <mikechaos13@gmail.com>
2015-05-11 15:35:55 -0400

    SF-6269 - Base setup to build white label dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 //initialize all of our variables
-var app, base, gulp, gutil, gulpSequence, shell, build_deps;
+var app, base, gulp, gutil, gulpSequence, shell, build_deps, path, through2, File;
 
 //load all of our dependencies
 //add more here if you want to include more libraries
-gulp        = require('gulp');
-gutil       = require('gulp-util');
-gulpSequence = require('gulp-sequence').use(gulp);
-shell       = require('gulp-shell');
+gulp          = require('gulp');
+gutil         = require('gulp-util');
+File          = gutil.File;
+path          = require('path');
+through2      = require('through2');
+gulpSequence  = require('gulp-sequence').use(gulp);
+shell         = require('gulp-shell');
 
 
 gulp.task('write-dep-file', shell.task([
@@ -15,8 +18,7 @@ gulp.task('write-dep-file', shell.task([
 ]));
 
 gulp.task('write-deps', function () {
-  return string_src("dependencies.js", 'test123')
-    .pipe(gulp.dest('./'))
+  build_deps.get_configs();
 });
 
 gulp.task('build-deps', gulpSequence(['write-dep-file', 'write-deps']));
@@ -31,8 +33,48 @@ function string_src(filename, string) {
 }
 
 build_deps = {
-  build_deps_from_string: function () {
-
+  get_configs: function () {
+    gulp.src('./configs/*.json')
+      .pipe(this.build_deps('test'))
+      .pipe(gulp.dest('./dependencies/'));
   },
+
+  build_deps: function(modifier) {
+    var self = this;
+    return through2.obj(function(file, encoding, done) {
+      var filename = path.basename(file.path);
+      var config = require('./configs/' + filename);
+
+      self.current_retailer = self.get_retailer(filename);
+      var dep_string = self.get_deps(config);
+
+      var target_name = self.get_dep_filename(filename);
+      var dep = new File({ cwd: "", base: "", path: target_name, contents: new Buffer(dep_string) });
+      done(null, dep);
+    });
+  },
+
+  get_deps: function (config) {
+    return JSON.stringify(config);
+  },
+
+  get_export_string: function (obj, component) {
+    var component_path = this.get_component_path(component)
+    return 'import App.' + obj.toLowerCase() + ' from ' + component_path;
+  },
+
+  get_component_path: function (component) {
+    var basepath = 'app/components/'
+    return  basepath + component + '/retailers/' + this.current_retailer + '/' + component + '.js';
+  },
+
+  get_retailer: function (filename) {
+    var reg = /^configs\.(\w+?).json$/
+    return reg.exec(filename)[1];
+  },
+
+  get_dep_filename: function (filename) {
+    return 'dependencies.' + this.current_retailer + '.js';
+  }
 
 }
