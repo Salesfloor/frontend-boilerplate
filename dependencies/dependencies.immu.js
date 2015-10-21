258c090fc7fc4b9460d6a95df3b70043b29dd990
Mikechaos <mikechaos13@gmail.com>
2015-10-21 18:18:45 -0400

    Remove dependencies

--- dependencies/dependencies.immu.js
+++
@@ -1,5 +0,0 @@
-import Page from 'app/components/page/page';
-import Header from 'app/components/header/header';
-
-
-export { Page, Header };
--- dependencies/dependencies.sw.js
+++
@@ -1,5 +0,0 @@
-import Page from 'app/components/page/page';
-import Header from 'app/components/header/retailers/sw/header';
-
-
-export { Page, Header };
